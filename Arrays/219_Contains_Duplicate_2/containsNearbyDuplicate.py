0d5cf871cfb8855827c1b83a40ed87395b292673
Wilson Chen <wilsonchen901@gmail.com>
2023-04-11 19:07:09 -0400

    complete leetcode 219

--- Arrays/219_Contains_Duplicate_2/containsNearbyDuplicate.py
+++ Arrays/219_Contains_Duplicate_2/containsNearbyDuplicate.py
@@ -14,3 +14,19 @@
 
 # Input: nums = [1,2,3,1,2,3], k = 2
 # Output: false
+
+def containsNearbyDuplicate(self, nums, k):
+    
+    window = set()
+    left = 0
+    
+    for right in range(len(nums)):
+        if right-left > k:
+            window.discard(nums[left])
+            left += 1
+        if nums[right] in window:
+            return True
+        window.add(nums[right])
+        
+    return False
+            
