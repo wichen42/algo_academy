d84dda9211b60752b0fb1642daa54c1e5bc05270
Wilson Chen <wilsonchen901@gmail.com>
2023-04-17 16:18:27 -0400

    start leetcode 380

--- Arrays/79_Word_search/exist.py
+++ Arrays/79_Word_search/exist.py
@@ -11,3 +11,43 @@
 # Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
 # Output: false
 
+def exist(board, word):
+    # base cases
+    #   1. if we find the word, return True
+    #       a. keep track of index. done when i == len(word)
+    #   2. False
+    #       a. out of bounds
+    #       b. letter does not match with word[i]
+    #       c. we have visited
+    
+    rows, cols = len(board), len(board[0])
+    visited = set()
+    
+    def _backtrack(r, c, i):
+        if i == len(word):
+            return True
+        
+        if not _inbound(r,c) or word[i] != board[r][c] or (r,c) in visited:
+            return False
+        
+        # modify
+        visited.add((r,c))
+        
+        # recursion
+        res = _backtrack(r+1, c, i+1) or _backtrack(r-1, c, i+1) or _backtrack(r, c+1, i+1) or _backtrack(r, c-1, i+1)
+        
+        # backtrack
+        visited.discard((r,c))
+        
+    def _inbound(r, c):
+        rowInbound = r >= 0 and r < rows
+        colInbound = c >= 0 and c < cols
+        
+        return rowInbound and colInbound
+    
+    for r in range(rows):
+        for c in range(cols):
+            if _backtrack(r, c, 0):
+                return True
+            
+    return False
