0fcb41dd3f479eed0c855c283e14fb62720422dc
Wilson Chen <wilsonchen901@gmail.com>
2023-04-18 15:17:37 -0400

    start leetcode 146

---
+++ Classes/146_LRU_Cache/LRU.py
@@ -0,0 +1,15 @@
+class LRUCache:
+    
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.DLL = new DoublyLinkedList()
+        self.hash = {}
+        
+class Node:
+    def __init__(self, key, val):
+        
+
+class DobulyLinkedList:
+    
+    def __init__(self):
+        
