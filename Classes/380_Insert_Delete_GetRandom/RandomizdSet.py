fffa0770700ff6956d88ba892273b145194108be
Wilson Chen <wilsonchen901@gmail.com>
2023-04-18 15:14:40 -0400

    complete

--- Classes/380_Insert_Delete_GetRandom/RandomizdSet.py
+++ Classes/380_Insert_Delete_GetRandom/RandomizdSet.py
@@ -1,10 +1,28 @@
 class RandomizedSet:
     
     def __init__(self):
+        self.map = {}
+        self.list = []
         
     def insert(self, val):
-    
+       if val in self.map:
+           return False
+       
+       self.list.append(val) 
+       self.map[val] = len(self.list)-1
+       return True
+        
     def remove(self, val):
-    
+        if val not in self.map:
+            return False
+        
+        lastElement = self.list[-1]
+        idx = self.map[val]
+        
+        self.list[idx], self.map[lastElement] = lastElement, idx
+        self.list.pop()
+        del self.map[val]
+        return True
+
     def getRandom(self):
-    
+        return random.choice(self.list)        
