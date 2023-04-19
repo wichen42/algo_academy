c514c1289b12e338879ff124c3132934f982e423
Wilson Chen <wilsonchen901@gmail.com>
2023-04-19 12:56:39 -0400

    start structy shortest path

---
+++ Graphs/Structy/shortest_path.py
@@ -0,0 +1,7 @@
+# Write a function, shortest_path that takes in a list of edgges for an undirected graph and two nodes (node_A, node_B). 
+# The function should return the length of the shortest path between A and B.
+# Consider the length as the number of edges in the path, not the number of nodes. If there is no path between A and B, then return -1.
+
+def shortest_path(edges, node_A, node_B):
+    
+    
